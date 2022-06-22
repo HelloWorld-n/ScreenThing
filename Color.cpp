1b90c9ed81476e20da69090e5a823ba9c8b6636d
HelloWorld-n <gajica.b@gmail.com>
2022-06-22 15:26:09 +0200

    first commit

---
+++ Color.cpp
@@ -0,0 +1,48 @@
+#include <string>
+
+class Color {
+	public:
+		short int red;
+		short int green;
+		short int blue;
+		
+		Color(short int red, short int green, short int blue){
+			this->red = red % 256;
+			this->green = green % 256;
+			this->blue = blue % 256;
+		}
+
+		Color(short int values[3], bool autoModuo = false){
+			for (int i = 0; i < 3; i++){
+				if ((values[i] > 255 or values[i] < 0) and (not autoModuo)){
+					throw "Please set `autoModuo` to `true` to allow values outside [0...255]"
+				}
+			}
+			this->red = values[0] % 256;
+			this->green = values[1] % 256;
+			this->blue = values[2] % 256;
+		}	
+
+		void setAll(short int values[3], bool autoModuo = false){
+			for (int i = 0; i < 3; i++){
+				if ((values[i] > 255 or values[i] < 0) and (not autoModuo)){
+					throw "Please set `autoModuo` to `true` to allow values outside [0...255]"
+				}
+			}
+			this->red = values[0] % 256;
+			this->green = values[1] % 256;
+			this->blue = values[2] % 256;
+		}	
+
+		int getRed(){
+			return this->red;
+		}
+
+		int getGreen(){
+			return this->green;
+		}
+
+		int getBlue(){
+			return this->blue;
+		}
+}
---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <thread>
+#include <random>
+#include <string>
+#include <vector>
+#include "TerminalUtil.cpp"
+
+int enlimit(int num, int limit){
+	if (num % (limit * 2) < limit){
+		return num % limit;
+	} else {
+		return limit - (num % limit);
+	}
+}
+
+int main(int argc, char **argv) {
+	int terminalSize[2] = {
+		WEXITSTATUS(std::system("exit `tput lines`")), 
+		WEXITSTATUS(std::system("exit `tput cols`"))
+	};
+
+	std::system("clear");
+	std::cout << std::flush;
+	
+	const int gradientStart = 0.2 * terminalSize[0];
+	const int gradientEnd = 0.7 * terminalSize[0];
+	const int brightest = 200;
+	int backgroundColor[3] = {};
+	for (int i = 0; i < terminalSize[0] - 2; i++){
+		for(int j = 0; j < terminalSize[1]; j++){
+			if (i < gradientStart){
+				for(int bg = 0; bg < 3; bg++){
+					backgroundColor[bg] = 0;
+				}
+			}
+			else if (i < gradientEnd){
+				for(int bg = 0; bg < 3; bg++){
+					backgroundColor[bg] = (
+						i - gradientStart
+					) * (
+						brightest / (gradientEnd - gradientStart)
+					);
+				}
+			} else {
+				backgroundColor[0] = 85 + (3 * enlimit(j, 15));
+				backgroundColor[1] = 0;
+				backgroundColor[2] = 255;
+			}
+
+			TerminalUtil::setBackgroundColor(backgroundColor);
+			std::cout << " ";
+		}
+		std::cout << "\n";
+	}
+	// reset colors
+	std::cout << "\e[0m\n";
+}
